ea047cc2e0edd3573801212b9343f4098935ac81
g.sen <giu.sene@gmail.com>
2022-03-05 10:10:21 +0100

    fix https request

--- src/libs/http.js
+++ src/libs/http.js
@@ -1,25 +1,24 @@
-const BASE_URL = 'http://api.football-data.org/v2';
+const BASE_URL = "https://api.football-data.org/v2";
 
-const httpGetLegue = (id) =>
-    fetch(BASE_URL + `/competitions/${id || 2019}/teams`, {
-        method: "GET",
-        headers: {
-            "X-Auth-Token": process.env.REACT_APP_FOOTBALL_API_KEY
-        }
-    }).then(response => response.json());
+const httpGetLegue = id =>
+  fetch(BASE_URL + `/competitions/${id || 2019}/teams`, {
+    method: "GET",
+    headers: {
+      "X-Auth-Token": process.env.REACT_APP_FOOTBALL_API_KEY,
+    },
+  }).then(response => response.json());
 
+const httpGetTeam = id =>
+  fetch(BASE_URL + "/teams/" + id, {
+    method: "GET",
+    headers: {
+      "X-Auth-Token": process.env.REACT_APP_FOOTBALL_API_KEY,
+    },
+  }).then(response => response.json());
 
-const httpGetTeam = (id) =>
-    fetch(BASE_URL + '/teams/' + id, {
-        method: "GET",
-        headers: {
-            "X-Auth-Token": process.env.REACT_APP_FOOTBALL_API_KEY
-        }
-    }).then(response => response.json());
+const httpGetImage = query =>
+  fetch(
+    `https://api.qwant.com/v3/search/images?count=1&q=${query}&t=images&safesearch=1&locale=it_IT&offset=0&device=desktop`
+  ).then(response => response.json());
 
-
-const httpGetImage = (query) =>
-    fetch(`https://api.qwant.com/v3/search/images?count=1&q=${query}&t=images&safesearch=1&locale=it_IT&offset=0&device=desktop`)
-        .then(response => response.json());
-
-export { httpGetLegue, httpGetTeam, httpGetImage }
+export { httpGetLegue, httpGetTeam, httpGetImage };
